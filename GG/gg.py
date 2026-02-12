d859e08ad22f462e277363eec597e26c27e7e048
giulianigabbo95 <giulianigabbo95@gmail.com>
2026-02-12 16:46:19 +0100

    Prova Commit Branch 2
    Prova per capire il funzionamento di un branch. 2

---
+++ GG/gg.py
@@ -0,0 +1,27 @@
+<<<<<<< Updated upstream
+=======
+'''
+Esercizio: Gestionale di un’Officina di Elettrodomestici
+Concetti obbligatori: incapsulamento, ereditarietà, polimorfismo, uso di type() per controllare
+gli oggetti e metodi “variatici” (con parametri variabili).
+Stai progettando il software di base per un’officina che si occupa di riparare elettrodomestici
+(lavatrici, frigoriferi, forni, ecc.). Il programma dovrà modellare gli elettrodomestici, i
+ticket di riparazione e alcune operazioni dell’officina, usando la programmazione ad oggetti.
+Classe base: Elettrodomestico
+Crea una classe base chiamata Elettrodomestico che rappresenti un elettrodomestico generico.
+Attributi (incapsulati, cioè privati):
+marca (stringa)
+modello (stringa)
+anno_acquisto (intero)
+guasto (stringa che descrive il problema principale)
+Metodi:
+Costruttore init(self, marca, modello, anno_acquisto, guasto)
+Getter e setter per tutti gli attributi, con eventuali controlli minimi (es. l’anno non può essere nel
+futuro).
+descrizione(self): restituisce una stringa con marca, modello, anno e guasto.
+stima_costo_base(self): restituisce un valore numerico generico (es. costo base di diagnosi).
+Regola 1 – Incapsulamento: tutti gli attributi devono essere privati (__attributo) e accessibili solo
+tramite getter/setter.
+'''
+aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+>>>>>>> Stashed changes
