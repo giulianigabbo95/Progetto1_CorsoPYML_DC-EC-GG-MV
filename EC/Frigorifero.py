8b552c1a67ca6bd42c0b823c553038cd1170f07d
eli-carella <eli.carella@gmail.com>
2026-02-13 16:01:37 +0100

    Create Frigorifero.py

---
+++ EC/Frigorifero.py
@@ -0,0 +1,19 @@
+##from gg import Elettrodomestico
+import Elettrodomestico from GG.Elettrodomestico
+
+
+class Frigorifero(Elettrodomestico):
+    
+    def __init__(self, litri, ha_freezer):
+        super().__init__()
+        self.litri = litri
+        self.ha_freezer = ha_freezer
+
+    def stima_costo_base(self):
+        costo = 35
+        if self.litri > 300:
+            costo += 25
+        if self.ha_freezer:
+            costo += 15
+        return costo
+    
